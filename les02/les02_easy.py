ecce93d862a8fc6e12d08308071037c376e5530c
AleksandrovichDm <kuzmin.stroy@gmail.com>
2019-05-05 21:15:21 +0300

    exersises 1

--- les02/les02_easy.py
+++ les02/les02_easy.py
@@ -13,13 +13,28 @@
 
 # Подсказка: воспользоваться методом .format()
 
-
-
+# fruit = ['banana', 'apple', 'melon', 'watermelon', 'lemon', 'orange', 'cherry', 'pineapple']
+# x = 0
+# while x < len(fruit) :
+#     print(x+1, '{0:>10}'.format(fruit[x]))
+#     x += 1
 
 
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
+
+list1 = ['True detective','Game of thrones', 'Breaking bad', 'Preason break', 'Friends']
+list2 = ['Black mirror','Game of thrones', 'Big bang theory', 'Method', 'Friends']
+
+result = list(set(list1) & set(list2))
+print(result)
+# y = 0
+# while y < len(result):
+#     list1.remove(y)
+#     i =+ 1
+#
+# print(list1)
 
 
 
