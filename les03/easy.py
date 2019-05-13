45eacdbae51e0d27910f7b3a4641d710f5ccee54
AleksandrovichDm <kuzmin.stroy@gmail.com>
2019-05-14 01:41:17 +0300

    well done easy

--- les03/easy.py
+++ les03/easy.py
@@ -5,26 +5,26 @@
 # Создайте функцию, принимающую на вход Имя, возраст и город проживания человека
 # Функция должна возвращать строку вида "Василий, 21 год(а), проживает в городе Москва"
 
-# def people(name, age, city):
-#     print(name, ', ', age, 'ears old, ', 'live in ', city)
-#
-# name = input('What is your name?')
-# age = input('How old are you?')
-# city = input('Where do you live?')
-#
-# people(name, age, city)
+def people(name, age, city):
+    print(name, ', ', age, 'ears old, ', 'live in ', city)
+
+name = input('What is your name?')
+age = input('How old are you?')
+city = input('Where do you live?')
+
+people(name, age, city)
 
 
 # Задание - 2
 # Создайте функцию, принимающую на вход 3 числа, и возвращающую наибольшее из них
 
-# def numbers (x, y, z):
-#     print(max(x, y, z))
-#
-# n1 = float(input('Input first number:'))
-# n2 = float(input('Input second number:'))
-# n3 = float(input('Input third number:'))
-# numbers(n1, n2, n3)
+def numbers (x, y, z):
+    print(max(x, y, z))
+
+n1 = float(input('Input first number:'))
+n2 = float(input('Input second number:'))
+n3 = float(input('Input third number:'))
+numbers(n1, n2, n3)
 
 
 # Задание - 3
@@ -32,7 +32,16 @@
 # верните самую длинную строку из полученных аргументов
 
 def text (*args):
-    return max(map(len, args))
+    maxlen = max(args, key = len)
+    return  maxlen
 
-data = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-print(text(data))
+
+data1 = 'Monday'
+data2 = 'Tuesday'
+data3 = 'Wednesday'
+data4 = 'Thursday'
+data5 = 'Friday'
+data6 = 'Saturday'
+data7 = 'Sunday'
+
+print(text(data1, data2, data3, data4, data5, data6, data7,))
